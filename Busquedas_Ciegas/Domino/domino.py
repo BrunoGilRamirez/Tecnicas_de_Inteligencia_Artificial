e23cd1889f8c63001a686767e83c9e0b5b4ad264
Bruno Gil Ramirez <106723899+BrunoGilRamirez@users.noreply.github.com>
2023-04-26 18:15:23 -0600

    Explicacion

--- Busquedas_Ciegas/Domino/domino.py
+++ Busquedas_Ciegas/Domino/domino.py
@@ -3,98 +3,102 @@ import time
 from maquina import jugador_Maquina as maquina
 
 class Domino:
-    def __init__(self):
-        self.fichas = [(i, j) for i in range(7) for j in range(i, 7)]
+    def __init__(self): #constructor de la clase Domino
+        self.fichas = [(i, j) for i in range(7) for j in range(i, 7)] #genera las fichas del juego
     
-    def generar_fichas(self):
-        return self.fichas.copy()
+    def generar_fichas(self): #genera las fichas del juego
+        return self.fichas.copy() #retorna las fichas del juego
     
-    def repartir_fichas(self):
+    def repartir_fichas(self): #reparte las fichas a los jugadores
         fichas_jugador_1 = []
         fichas_jugador_2 = []
         
-        fichas_disponibles = self.generar_fichas()
-        random.shuffle(fichas_disponibles)
+        fichas_disponibles = self.generar_fichas() #se generan las fichas del juego
+        random.shuffle(fichas_disponibles) #se mezclan las fichas del juego
         
         while len(fichas_disponibles) > 0:
-            fichas_jugador_1.append(fichas_disponibles.pop())
-            fichas_jugador_2.append(fichas_disponibles.pop())
+            fichas_jugador_1.append(fichas_disponibles.pop()) #se reparten las fichas al jugador 1
+            fichas_jugador_2.append(fichas_disponibles.pop()) #se reparten las fichas al jugador 2
         
         return fichas_jugador_1, fichas_jugador_2
         
 class Juego_Domino:
-    def __init__(self, fichas_jugador_1, fichas_jugador_2):
+    def __init__(self, fichas_jugador_1, fichas_jugador_2): #constructor de la clase Juego_Domino
         self.tablero = []
-        self.fichas_jugador_1 = fichas_jugador_1
-        self.fichas_jugador_2 = fichas_jugador_2
-        self.ficha_central = (6, 6)
-        self.turno = 0
+        self.fichas_jugador_1 = fichas_jugador_1 #fichas del jugador 1
+        self.fichas_jugador_2 = fichas_jugador_2 #fichas del jugador 2
+        self.ficha_central = (6, 6) #ficha central del juego
+        self.turno = 0 #turno del juego
+
     def decidir_turno(self):
-        if  self.ficha_central in self.fichas_jugador_1:
+        if  self.ficha_central in self.fichas_jugador_1: #si la ficha central esta en las fichas del jugador 1
+            return 1 #retorna 1
+        elif self.ficha_central in self.fichas_jugador_2: #si la ficha central esta en las fichas del jugador 2
+            return 2 #retorna 2
+        
+    def determinar_ganador(self,mesa, fichas_jugador_1, fichas_jugador_2, skip): #determina el ganador del juego
+        if len(fichas_jugador_1) == 0:  #si el jugador 1 no tiene fichas, gana el jugador 1
             return 1
-        elif self.ficha_central in self.fichas_jugador_2:
+        elif len(fichas_jugador_2) == 0: #si el jugador 2 no tiene fichas, gana el jugador 2
             return 2
-    def determinar_ganador(self,mesa, fichas_jugador_1, fichas_jugador_2, skip):
-        if len(fichas_jugador_1) == 0:
-            return 1
-        elif len(fichas_jugador_2) == 0:
-            return 2
-        elif skip>=2:
-            suma_fichas_jugador_1 = sum(sum(ficha) for ficha in fichas_jugador_1)
-            suma_fichas_jugador_2 = sum(sum(ficha) for ficha in fichas_jugador_2)
-            if suma_fichas_jugador_1 < suma_fichas_jugador_2:
+        elif skip>=2: #si los dos jugadores no pueden jugar, gana el jugador con menos fichas
+            suma_fichas_jugador_1 = sum(sum(ficha) for ficha in fichas_jugador_1) #suma las fichas del jugador 1
+            suma_fichas_jugador_2 = sum(sum(ficha) for ficha in fichas_jugador_2) #suma las fichas del jugador 2
+            if suma_fichas_jugador_1 < suma_fichas_jugador_2: #si la suma de las fichas del jugador 1 es menor a la suma de las fichas del jugador 2, gana el jugador 1
                 return 1
             else:
-                return 2
+                return 2 #si la suma de las fichas del jugador 2 es menor a la suma de las fichas del jugador 1, gana el jugador 2
         else:
             return None
-    def string_to_tuple(self, string):
+        
+    def string_to_tuple(self, string): #convierte un string a una tupla
         val=-1
         try:
             string.replace("(", "")
-            string.replace(")", "")
+            string.replace(")", "") 
             val=tuple(map(int, string.split(",")))
         except:
             pass
         return val
-    def jugar(self):
-        maquina1 = maquina(self.fichas_jugador_2, self.tablero)
-        turno = self.decidir_turno()
-        if turno == 1:
+    
+    def jugar(self): #funcion que inicia el juego
+        maquina1 = maquina(self.fichas_jugador_2, self.tablero) #se crea la maquina
+        turno = self.decidir_turno() #se decide el turno
+        if turno == 1: #si el turno es 1, empieza el jugador 1
             print("Empieza el jugador 1")
-            self.tablero.append(self.ficha_central), self.fichas_jugador_1.remove(self.ficha_central)
-            turno = 2
-        elif turno == 2:
+            self.tablero.append(self.ficha_central), self.fichas_jugador_1.remove(self.ficha_central) #se agrega la ficha central al tablero y se elimina de las fichas del jugador 1
+            turno = 2 #turno de la maquina
+        elif turno == 2: #si el turno es 2, empieza el jugador 2
             print("Empieza la computadora")
-            self.tablero.append(self.ficha_central), self.fichas_jugador_2.remove(self.ficha_central)
-            turno = 1
-        self.skip=0
-        while (ganador := self.determinar_ganador(self.tablero, self.fichas_jugador_1, self.fichas_jugador_2,self.skip)) is None:
+            self.tablero.append(self.ficha_central), self.fichas_jugador_2.remove(self.ficha_central) #se agrega la ficha central al tablero y se elimina de las fichas del jugador 2
+            turno = 1 #turno del jugador 1
+        self.skip=0 #contador de turnos sin jugar
+        while (ganador := self.determinar_ganador(self.tablero, self.fichas_jugador_1, self.fichas_jugador_2,self.skip)) is None: #mientras no haya un ganador
             print("--------------------------------------Tablero--------------------------------------------")
             print(f"\n\nFichas en la mesa: {self.tablero}\n\n")
             print("--------------------------------------Tablero--------------------------------------------\n")
             time.sleep(2)
-            if turno == 1:
+            if turno == 1: #turno del jugador 1
                 print("Turno del jugador 1")
                 print(f"Fichas del jugador 1: {self.fichas_jugador_1}")
                 ficha = input("Ingrese la ficha que desea jugar: ")
                 ficha = self.string_to_tuple(ficha)
                 if ficha==-1:
                     self.skip+=1
-                if ficha in self.fichas_jugador_1:
+                if ficha in self.fichas_jugador_1: #si la ficha esta en las fichas del jugador 1
                     if ficha[0]==self.tablero[0][0] :
-                        self.tablero.insert(0, ficha[::-1])
+                        self.tablero.insert(0, ficha[::-1]) #se invierte la ficha y se agrega al tablero
                         self.fichas_jugador_1.remove(ficha)
                         self.skip=0
-                    elif ficha[1]==self.tablero[0][0]: 
+                    elif ficha[1]==self.tablero[0][0]: #si el primer numero de la ficha es igual al primer numero de la ficha del tablero
                         self.tablero.insert(0, ficha)
                         self.fichas_jugador_1.remove(ficha)
                         self.skip=0
-                    elif ficha[1]==self.tablero[-1][1]:
-                        self.tablero.append(ficha[::-1])
+                    elif ficha[1]==self.tablero[-1][1]: #si el segundo numero de la ficha es igual al segundo numero de la ficha del tablero
+                        self.tablero.append(ficha[::-1]) #se invierte la ficha y se agrega al tablero
                         self.fichas_jugador_1.remove(ficha)
                         self.skip=0
-                    elif ficha[0]==self.tablero[-1][1]:
+                    elif ficha[0]==self.tablero[-1][1]: #si el primer numero de la ficha es igual al segundo numero de la ficha del tablero
                         self.tablero.append(ficha)
                         self.fichas_jugador_1.remove(ficha)
                         self.skip=0
@@ -106,13 +110,13 @@ class Juego_Domino:
             elif turno == 2:
                 print("\nTurno de la computadora")
                 print(f"Fichas de la maquina: {self.fichas_jugador_2}")
-                ficha = maquina1.jugar(self.fichas_jugador_2, self.tablero)
+                ficha = maquina1.jugar(self.fichas_jugador_2, self.tablero) #se obtiene la ficha que jugara la maquina
                 print(f"La computadora jugo la ficha {ficha}\n")
                 time.sleep(2)
-                if ficha==-1:
-                    self.skip+=1
+                if ficha==-1: #si la ficha es -1, la maquina no puede jugar
+                    self.skip+=1 #se aumenta el contador de turnos sin jugar
                 if ficha in self.fichas_jugador_2:
-                    if ficha[0]==self.tablero[0][0] :
+                    if ficha[0]==self.tablero[0][0] : 
                         self.tablero.insert(0, ficha[::-1])
                         self.fichas_jugador_2.remove(ficha)
                         self.skip=0
@@ -137,11 +141,10 @@ class Juego_Domino:
         print(f"El ganador es el jugador {ganador}")
 
         
-domino = Domino()
-jugador1, jugador2 = domino.repartir_fichas()
-#print(f"jugador 1: {jugador1} \njugador 2: {jugador2}")
-juego = Juego_Domino(jugador1, jugador2)
-juego.jugar()
+domino = Domino() #se crea el objeto domino
+jugador1, jugador2 = domino.repartir_fichas() #se reparten las fichas
+juego = Juego_Domino(jugador1, jugador2) #se crea el objeto juego
+juego.jugar() #se inicia el juego
 
 
 
--- Busquedas_Ciegas/Domino/maquina.py
+++ Busquedas_Ciegas/Domino/maquina.py
@@ -1,41 +1,43 @@
 class jugador_Maquina:
     tablero = []
     fichas = []
-    def __init__(self, fichas, tablero):
-        self.fichas = fichas
-        self.tablero = tablero
-    def actualizar_tablero(self, tablero):
-        self.tablero = tablero
-    def actualizar_fichas(self, fichas):
-        self.fichas = fichas
+    #Algortimo utilizado: Best First Search
+    def __init__(self, fichas, tablero): #constructor de la clase jugador_Maquina
+        self.fichas = fichas #inicializa las fichas del jugador
+        self.tablero = tablero #inicializa el tablero del jugador
+    def actualizar_tablero(self, tablero): #actualiza el tablero del jugador
+        self.tablero = tablero 
+    def actualizar_fichas(self, fichas): #actualiza las fichas del jugador
+        self.fichas = fichas #debido a que es el juego quien elimina las fichas, se actualizan las fichas del jugador
     
-    def mejor_ficha(self, fichas):
-        mejor_ficha = fichas[0]
-        for ficha in fichas:
-            if ficha[0]+ficha[1] > mejor_ficha[0]+mejor_ficha[1]:
-                mejor_ficha = ficha
-        return mejor_ficha
+    def mejor_ficha(self, fichas): #retorna la mejor ficha para jugar, Se considera que esta es la funcion que implementa la heuristica, de que la ficha de mayor valor es la que se debe jugar
+        mejor_ficha = fichas[0] #se inicializa la mejor ficha como la primera ficha de la lista de fichas
+        for ficha in fichas: #se recorre la lista de fichas
+            if ficha[0]+ficha[1] > mejor_ficha[0]+mejor_ficha[1]: #si la suma de los valores de la ficha actual es mayor a la suma de los valores de la mejor ficha, se actualiza la mejor ficha
+                mejor_ficha = ficha #se actualiza la mejor ficha
+        return mejor_ficha #se retorna la mejor ficha
 
-    def fichas_posibles(self, Valorderecho, Valorizquierdo):
-        fichas_posibles = []
-        for ficha in self.fichas:
-            if ficha[0] == Valorderecho or ficha[1] == Valorderecho:
-                fichas_posibles.append(ficha)
-            elif ficha[0] == Valorizquierdo or ficha[1] == Valorizquierdo:
-                fichas_posibles.append(ficha)
-        return fichas_posibles
-    def jugar(self,fichas,tablero):
-        self.actualizar_fichas(fichas)
-        self.actualizar_tablero(tablero)
-        if len(self.fichas)==0:
-            return -1
+    def fichas_posibles(self, Valorderecho, Valorizquierdo): #retorna las fichas que se pueden jugar en el tablero, esta es considerada la funcion sucessora
+        fichas_posibles = [] #se inicializa la lista de fichas posibles
+        for ficha in self.fichas: #se recorre la lista de fichas
+            if ficha[0] == Valorderecho or ficha[1] == Valorderecho: #si el valor derecho de la ficha es igual al valor derecho del tablero o el valor izquierdo de la ficha es igual al valor derecho del tablero, se agrega la ficha a la lista de fichas posibles
+                fichas_posibles.append(ficha) #se agrega la ficha a la lista de fichas posibles
+            elif ficha[0] == Valorizquierdo or ficha[1] == Valorizquierdo: #si el valor izquierdo de la ficha es igual al valor izquierdo del tablero o el valor derecho de la ficha es igual al valor izquierdo del tablero, se agrega la ficha a la lista de fichas posibles
+                fichas_posibles.append(ficha) #se agrega la ficha a la lista de fichas posibles
+        return fichas_posibles #se retorna la lista de fichas posibles
+    
+    def jugar(self,fichas,tablero): #retorna la mejor ficha para jugar, esta es considerada la funcion objetivo
+        self.actualizar_fichas(fichas) #se actualizan las fichas del jugador
+        self.actualizar_tablero(tablero) #se actualiza el tablero del jugador
+        if len(self.fichas)==0: #si el jugador no tiene fichas, retorna -1, hace una especie de funcion objetivo, que si no tiene fichas, retorna -1 y el juego termina
+            return -1   
+    
+        Valorderecho = self.tablero[len(self.tablero)-1][1] #se obtiene el valor derecho del tablero
+        Valorizquierdo = self.tablero[0][0] #se obtiene el valor izquierdo del tablero
+        fichas_posibles = self.fichas_posibles(Valorderecho, Valorizquierdo) #se obtienen las fichas posibles
         
-        Valorderecho = self.tablero[len(self.tablero)-1][1]
-        Valorizquierdo = self.tablero[0][0]
-        fichas_posibles = self.fichas_posibles(Valorderecho, Valorizquierdo)
-        
-        if len(fichas_posibles) == 0:
-            return -1
+        if len(fichas_posibles) == 0: #si no hay fichas posibles, retorna -1, y se toma como un pasar turno, si ambos jugadores pasan turno, el juego termina
+            return -1 
         else:
-            return self.mejor_ficha(fichas_posibles)
+            return self.mejor_ficha(fichas_posibles) #si hay fichas posibles, retorna la mejor ficha para jugar
            
